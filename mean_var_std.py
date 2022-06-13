57171cd97d030868bbf88294f0e72ffc989efa6d
jayant96 <>
2022-06-13 11:00:11 +0200

    To implement statistics using numpy

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
-    return calculations
+  if len(list) != 9:
+    raise ValueError("List must contain nine numbers.")
+  arr = np.array(list)
+  arr_matrix = arr.reshape((3,3))
+  calculations = {
+  'mean': [np.mean(arr_matrix, axis = 0).tolist(),np.mean(arr_matrix, axis = 1).tolist(),np.mean(arr_matrix).tolist()],
+  'variance': [np.var(arr_matrix, axis=0).tolist(),np.var(arr_matrix, axis=1).tolist(),np.var(arr_matrix).tolist()],
+  'standard deviation': [np.std(arr_matrix, axis=0).tolist(),np.std(arr_matrix, axis=1).tolist(),np.std(arr_matrix).tolist()],
+  'max': [np.max(arr_matrix, axis=0).tolist(),np.max(arr_matrix, axis=1).tolist(),np.max(arr_matrix).tolist()],
+  'min': [np.min(arr_matrix, axis=0).tolist(),np.min(arr_matrix, axis=1).tolist(),np.min(arr_matrix).tolist()],
+  'sum': [np.sum(arr_matrix, axis=0).tolist(),np.sum(arr_matrix, axis=1).tolist(),np.sum(arr_matrix).tolist()]
+  }
+  return calculations
